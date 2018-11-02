ee079c5f6be2cda29bd16c10fb6bbc4f6a31f407
Tomek Lewicki <t.w.lewicki@gmail.com>
2018-11-02 16:48:53 +0100

    removed prints, wrapped stuff in mainloop function

--- send.py
+++ send.py
@@ -1,47 +1,40 @@
 import requests
 import json
 import time
+import logging
 
 #from requests import ConnectionError
 from pms7003.pms7003 import Pms7003Sensor, PmsSensorExcpetion
+
+def mainloop():
+    try:
+        j['values'] = sensor.read()
+    except PmsSensorExcpetion as e:
+        logging.exception('{} {}'.format(int(1000*time.time()), e))
+
+    j['timestamp'] = time.time()
+
+    try:
+        r=requests.post('https://smog-monitor.herokuapp.com/api/get-one', json=json.dumps(j), timeout=4)
+        logging.info('{} {}'.format(int(1000*time.time()), r.status_code))
+        if r.status_code != 200:
+            time.sleep(1)
+        del r
+
+    except ConnectionError as e:
+        logging.exception('{} {}'.format(int(1000*time.time()), e))
 
 if __name__ == '__main__':
 
     sensor = Pms7003Sensor('/dev/serial0')
     j = {}
+    logging.basicConfig(filename='/home/pi/smog-client-2/smog.log', level='INFO')
 
     while True:
-        try:
-            j['values'] = sensor.read()
-        except PmsSensorExcpetion:
-            print('Connection problem') 
+        mainloop()
+
+
+
+
+
         
-        j['timestamp'] = time.time()
-
-        try:
-            r=requests.post('https://smog-monitor.herokuapp.com/api/get-one', json=json.dumps(j))
-            print(r.status_code)
-            if r.status_code != 200:
-                time.sleep(1)
-        except ConnectionError:
-            print('Network connection problem')
-
-
-
-
-            
-
-
-# if __name__ == '__main__':
-    
-# 	dump=json.dumps(list_of_tuples)
-	
-# 	try:
-# 		r=requests.post('http://46.101.98.211/hehe/1234',json=dump)
-# 		list_of_tuples=[]
-# 		logging.info('batch succeded with code:'.format(r.status_code))
-		
-# 	except (requests.exceptions.ConnectionError, ConnectionRefusedError) as e:
-# 		logging.error('batch failed with error:'.format(e))
-	
-# 	return list_of_tuples
