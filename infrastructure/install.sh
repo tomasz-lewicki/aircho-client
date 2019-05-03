bae0aaa54a4adb1a21f1e8fa9c9fe89296d1acc9
Tomasz Lewicki <t.w.lewicki@gmail.com>
2019-05-03 07:54:43 +0100

    enabled running as a service

--- infrastructure/install.sh
+++ infrastructure/install.sh
@@ -3,3 +3,5 @@ sudo apt-get install git -y
 sudo apt-get install vim -y
 pip3 install -r requirements.txt
 mv lenovo_x230.pub ~/.ssh/authorized_keys
+sudo cp aircho.service /etc/systemd/system
+sudo systemctl enable aircho.service
